dbfa5e1f2a86188339d236acbe06e7b9b0f45e33
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2024-02-08 21:34:55 +0100

    complete

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
+const key = "bByPReOKKldzbYCKBsEsa7FCjBr2epG8CzQnBAV8ICc"
+
 const searchForm = document.getElementById("search-form")
 const searchBox = document.getElementById("search-box")
 const searchResult = document.getElementById("search-result")
-const btnShowMore = document.getElementById("show-more")
-import { config } from "./config"
+const btnShowMore = document.getElementById("show-more-btn")
 
-let query = new String()
-let page = new Number()
+let query = ``
+let page = 1
 
-const getData = async () => {
+async function getImages() {
   query = searchBox.value
-  const res = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}$client_id=${config.IMG_KEY}`)
-  !res.ok && "Error fetching API"
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${key}&per_page=12`
+  const res = await fetch(url)
   const data = await res.json()
-  console.log(data)
+
+  if (page === 1) {
+    searchResult.innerHTML = ""
+  }
+
+  const results = data.results
+
+  results.map((result) => {
+    const image = document.createElement("img")
+    image.src = result.urls.small
+    const imgLink = document.createElement("a")
+    imgLink.href = result.links.html
+    imgLink.target = "_blank"
+
+    imgLink.appendChild(image)
+    searchResult.appendChild(imgLink)
+  })
+  btnShowMore.style.display = "block"
+  //console.log(data)
 }
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  page = 1
+  getImages()
+})
+
+btnShowMore.addEventListener("click", () => {
+  page++
+  getImages()
+})
